4e8140502bd1387dcc5a833d09e336bb172da1cc
Paul Popa <paul.popa@moduscreate.com>
2021-08-23 02:43:09 +0300

    configured env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,21 @@ import admin, { ServiceAccount } from 'firebase-admin';
 
 import serviceAccount from '../cv-builder-4a6bd-firebase-adminsdk-tszt2-5f7c26834f.json';
 
+const keys = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  credential: admin.credential.cert((keys || serviceAccount) as ServiceAccount),
   storageBucket: 'cv-builder-4a6bd.appspot.com',
 });
 
