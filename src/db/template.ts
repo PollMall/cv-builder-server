67cc7b768eb0c47ea6f397b274f7feefd0f8f61e
Paul Popa <paul.popa@moduscreate.com>
2022-10-12 15:47:18 +0300

    put back networkidle0

--- src/db/template.ts
+++ src/db/template.ts
@@ -23,7 +23,7 @@ const createPDFFromHTML = async (html: string) => {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
-  await page.setContent(html);
+  await page.setContent(html, { waitUntil: 'networkidle0' });
   const pdf = await page.pdf({ format: 'letter', printBackground: true });
   await browser.close();
   return pdf;
